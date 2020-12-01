b3752e048fc2d6e42bf88b768e4f240f1b6e61c9
Mike MacMartin <mmacmartin@gmail.com>
2020-12-01 00:25:35 -0500

    Add the example bar from Material-UI. The vertical placement is now broken for the main content.

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import ReactDOM from 'react-dom';
 import {
     Switch,
@@ -10,11 +11,16 @@ import {
     useLocation
 } from "react-router-dom";
 
-import { AppBar, IconButton, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, IconButton, Toolbar, Typography, CssBaseline, Drawer } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
+import { Divider, List, ListItem, ListItemText, ListItemIcon } from '@material-ui/core';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 
 const NoRoute = (props) => {
-    return (<Typography variant="h1">Hello World!</Typography>);
+    return (<Typography variant="h1" paragraph>Hello World!</Typography>);
 };
 
 const Article = (props) => {
@@ -23,47 +29,169 @@ const Article = (props) => {
 };
 
 // TODO: use theme
+const drawerWidth = 240;
+
 const useStyles = makeStyles((theme) => ({
     root: {
-        flexGrow: 1,
+        display: 'flex',
+    },
+    appBar: {
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
     },
     menuButton: {
         marginRight: theme.spacing(2),
     },
-    title: {
+    hide: {
+        display: 'none',
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    drawerHeader: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: theme.spacing(0, 1),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar,
+        justifyContent: 'flex-end',
+    },
+    offset: theme.mixins.toolbar,
+    content: {
         flexGrow: 1,
+        padding: theme.spacing(3),
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        marginLeft: -drawerWidth,
+    },
+    contentShift: {
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginLeft: 0,
     },
 }));
 
-const App = (props) => {
+const TitleBar = (props) => {
     const classes = useStyles();
+    const theme = useTheme();
+    const { drawerIsOpen, setDrawerIsOpen, ...rest } = props;
     const location = useLocation();
 
-    return (<>
-        <div className={classes.root}>
-            <AppBar position="fixed">
-                <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography classname={classes.title} variant="h6">
-                        {location.pathname}
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-            <Toolbar /> {/* this fixes the "fixed" spacing per Material UI docs. */}
+    const handleDrawerOpen = () => {
+        setDrawerIsOpen(true);
+    };
+
+    return (<React.Fragment>
+        <AppBar position="fixed" className={clsx(classes.appBar, {
+            [classes.appBarShift]: drawerIsOpen,
+        })}>
+            <Toolbar>
+                <IconButton
+                    color="inherit"
+                    aria-label="open drawer"
+                    onClick={handleDrawerOpen}
+                    edge="start"
+                    className={clsx(classes.menuButton, drawerIsOpen && classes.hide)}
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" noWrap>
+                    {location.pathname}
+                </Typography>
+            </Toolbar>
+        </AppBar>
+    </React.Fragment>);
+};
+
+const NavBar = (props) => {
+    const classes = useStyles();
+    const theme = useTheme();
+    const { drawerIsOpen, setDrawerIsOpen, ...rest } = props;
+    const location = useLocation();
+
+    const handleDrawerClose = () => {
+        setDrawerIsOpen(false);
+    };
+
+    return (<Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={drawerIsOpen}
+        classes={{
+            paper: classes.drawerPaper,
+        }}
+    >
+        <div className={classes.drawerHeader}>
+            <IconButton onClick={handleDrawerClose}>
+                {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+            </IconButton>
         </div>
-        {/* do a nav bar on the left hand side - in a side menu bar */}
+        <Divider />
+        <List>
+            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                <ListItem button key={text}>
+                    <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                    <ListItemText primary={text} />
+                </ListItem>
+            ))}
+        </List>
+        <Divider />
+        <List>
+            {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                <ListItem button key={text}>
+                    <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                    <ListItemText primary={text} />
+                </ListItem>
+            ))}
+        </List>
+    </Drawer>);
+};
+
+const App = (props) => {
+    const classes = useStyles();
+    const theme = useTheme();
+    const [drawerIsOpen, setDrawerIsOpen] = React.useState(false);
+
+    return (<div className={classes.root}>
+        <CssBaseline />
+        <TitleBar drawerIsOpen={drawerIsOpen} setDrawerIsOpen={setDrawerIsOpen} />
+        <NavBar drawerIsOpen={drawerIsOpen} setDrawerIsOpen={setDrawerIsOpen} />
         {/* Main body is the router switch */}
-        <Switch>
-            <Route path="/article/:articleId">
-                <Article />
-            </Route>
-            <Route>
-                <NoRoute />
-            </Route>
-        </Switch>
-    </>);
+        <div className={classes.offset} />
+        <main
+            className={clsx(classes.content, {
+                [classes.contentShift]: drawerIsOpen,
+            })}
+        >
+            <Switch>
+                <Route path="/article/:articleId">
+                    <Article />
+                </Route>
+                <Route>
+                    <NoRoute />
+                </Route>
+            </Switch>
+        </main>
+    </div>);
 };
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.querySelector("#root"));
